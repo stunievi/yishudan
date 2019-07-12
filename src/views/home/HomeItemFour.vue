c82a158e9e107c7885bf5d2e9a8a7ef8dd8bedf7
stunievi <ievi@live.com>
2019-07-13 00:59:23 +0800

    增加下载图书分类

--- src/views/home/HomeItemFour.vue
+++ src/views/home/HomeItemFour.vue
@@ -4,6 +4,12 @@
     <!-- <Index class="ppp" msg='推荐书单'/> -->
     <div v-if="statu">
       <div class="cards">
+        <!-- 分类id不为零显示返回 -->
+        <div v-if="cate_id" @click="backButton()">
+          <Card :padding="20" class="card-box" >
+            <h4>返回</h4>
+          </Card>
+        </div>
         <div v-for="list in cate_list" :key= "list.id" @click='getSonCate(list.id)'>
           <Card :padding="20" class="card-box" >
             <h4>{{list.cate_name}}</h4>
@@ -29,6 +35,8 @@ export default {
       // index: 0,
       cate_list: [],
       statu: 0,
+      cate_id: 0,
+      cate_arr: [0],
       networkStatu: true,
     }
   },
@@ -46,8 +54,22 @@ export default {
       })
     },
     getSonCate: function (id) {
-      console.log(id)
+      // console.log(id)
+      this.cate_id = id
+      this.cate_arr.push(id)
       this.getCateList('?id=' + id)
+    },
+    // 每前进一步数组追加分类id，后退一步数组删除一位
+    // 然后根据数组最后一位分类id查询展示分类。
+    backButton: function (forward = null) {
+      this.cate_arr.pop()
+      var len = this.cate_arr[this.cate_arr.length-1]
+      if (len == undefined || len == 0) {
+        this.getCateList('?id=' + len)
+        this.cate_id = 0
+      }else{
+        this.getCateList('?id=' + len)
+      }
     }
   }
 }
